8d803971879dd8324b6b913999eb9a900691dae0
arcgate-sanah <sanah.mathur@arcgate.com>
2023-10-04 12:34:43 +0530

    searching,filtering done with testcases

--- arc_learning_platform/urls.py
+++ arc_learning_platform/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -22,11 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("account.apis.urls")),
     # Swagger urls
-    re_path(
-        r"^swagger(?P<format>\.json|\.yaml)$",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
     path(
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
